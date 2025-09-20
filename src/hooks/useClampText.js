484708ba3f2d9b7dffa2ddc31126a11669b8dfba
Saumya Singh <167208781+Saumyas18s@users.noreply.github.com>
2025-09-20 12:29:38 +0530

    Update useClampText.js

--- src/hooks/useClampText.js
+++ src/hooks/useClampText.js
@@ -1,1 +1,16 @@
+import { useEffect, useRef } from "react";
 
+export default function useClampText(lines = 3) {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      elementRef.current.style.display = "-webkit-box";
+      elementRef.current.style.webkitBoxOrient = "vertical";
+      elementRef.current.style.webkitLineClamp = lines;
+      elementRef.current.style.overflow = "hidden";
+    }
+  }, [lines]);
+
+  return elementRef;
+}
